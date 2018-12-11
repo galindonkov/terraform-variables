7fc6fb1aa97d53c0ddd4881cb2dd79989c3dab98
galindonkov <43785350+galindonkov@users.noreply.github.com>
2018-12-11 16:08:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 variable "best_soccers" {
   type        = "list"
   description = "List of the best soccers of all time"
-  default     = ["Pele", "Ronaldo", "Mesi", "Maradona", "Gundi"]
+  default     = "Pele", "Ronaldo", "Mesi", "Maradona", "Gundi"
 }
 
 output "Best players of all time" {
